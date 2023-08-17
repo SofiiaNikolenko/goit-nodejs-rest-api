031f6f397b6112314fe848dcec634bddf3a5576a
SofiiaNikolenko <sofii.nikolenko@gmail.com>
2023-08-17 17:07:02 +0200

    fixing problems

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,9 +2,15 @@ const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
   const func = (req, res, next) => {
+    if (req.method === "PUT" && Object.keys(req.body).length === 0) {
+      return next(HttpError(400, "missing fields"));
+    }
+
     const { error } = schema.validate(req.body);
     if (error) {
-      next(HttpError(400, error.message));
+      const missingField = error.details[0].context.label;
+      const errorMessage = `missing required ${missingField} field`;
+      return next(HttpError(400, errorMessage));
     }
     next();
   };
